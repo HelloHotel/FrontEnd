d4489a0da0b73d414e806934da4a72aeb1b3f6ba
JosueCQ <44213146+JosueCQ@users.noreply.github.com>
2021-10-17 12:00:43 -0500

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Home_App></Home_App>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+import Home_App from "../components/Home_App";
+export default {
+  name: 'Home',
+  components: {
+    Home_App
+
+  }
+}
+</script>
