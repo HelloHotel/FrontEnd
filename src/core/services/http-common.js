35a819e2c8c96ba3f2f4b77503e2aea92661be24
Alonso-C30 <u201924282@upc.edu.pe>
2021-10-28 22:18:03 -0500

    Correction http-common.js
    misspelled "headers"

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
 
     baseURL: "http://localhost:3000/api",
-    headers: {"Content-type": "appication/json"}
+    headers: {"Content-type": "application/json"}
 });
